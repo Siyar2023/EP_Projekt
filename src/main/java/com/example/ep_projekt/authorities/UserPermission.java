8bc499b14229b48dec956c00ce8b4ce13eae8683
unknown <matteus.vinasco@stud.sti.se>
2024-12-28 19:42:02 +0100

    Add UserPermission enum to define user permissions for HTTP methods (GET, POST, PUT, DELETE)

--- src/main/java/com/example/ep_projekt/authorities/UserPermission.java
+++ src/main/java/com/example/ep_projekt/authorities/UserPermission.java
@@ -1,4 +1,18 @@
-package com.example.ep_projekt.authorities;
+package com.example.EP_Projekt.authorities;
 
-public class UserPermission {
+public enum UserPermission {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    private final String permission;
+
+    UserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission () {
+        return permission;
+    }
 }
