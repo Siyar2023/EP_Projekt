e1f14adf5150dbfeaef682c3eb871e0f0c2d405c
unknown <matteus.vinasco@stud.sti.se>
2024-12-28 19:59:07 +0100

    Add UserRole enum with permissions and authority mapping logic for security roles (GUEST, USER, ADMIN)

--- src/main/java/com/example/ep_projekt/authorities/UserRole.java
+++ src/main/java/com/example/ep_projekt/authorities/UserRole.java
@@ -1,4 +1,41 @@
-package com.example.ep_projekt.authorities;
+package com.example.projekt_arbete.authorities;
 
-public class UserRole {
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.projekt_arbete.authorities.UserPermission.*;
+
+public enum UserRole {
+    GUEST(List.of(GET.getPermission())),
+    USER(List.of(GET.getPermission(), POST.getPermission())),
+    ADMIN(List.of(GET.getPermission(), POST.getPermission(), DELETE.getPermission(), PUT.getPermission()));
+
+    private final List<String> permission;
+
+    UserRole(List<String> permission) {
+        this.permission = permission;
+    }
+
+    public List<String> getPermission () {
+        return permission;
+    }
+
+    public List<SimpleGrantedAuthority> getAuthorities () {
+
+        // TODO - Roles
+        // TODO - Permissions
+        // TODO - Create List Authority (concat both roles & perms)
+
+        List<SimpleGrantedAuthority> simpleGrantedAuthorityList = new ArrayList<>(
+
+        );
+
+        simpleGrantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+        simpleGrantedAuthorityList.addAll(getPermission().stream().map(SimpleGrantedAuthority::new).toList());
+
+        return simpleGrantedAuthorityList;
+    }
 }
